81b65cf13861c95d12e28d7c57f9a5596fa00296
Russell Williams <russellwilliamscc@gmail.com>
2016-06-22 23:30:20 +0100

    adds 'delete', 'edit' and 'find by id'

--- console.rb
+++ console.rb
@@ -8,24 +8,25 @@ require('pry-byebug')
 runner = SqlRunner.new( {dbname:'petshop', host:'localhost'})
 
 
-petshop1 = Petshop.new( {'name' => 'Fantastic Pet Emporium - Edinburgh'}, runner )
-petshop2 = Petshop.new( {'name' => 'Fantastic Pet Emporium - Newcastle'}, runner )
-petshop3 = Petshop.new( {'name' => 'Fantastic Pet Emporium - New York'}, runner )
+petshop1 = Petshop.new( {'name' => 'the dainty wee bijou Pet Emporium', 'type' => 'compact'}, runner )
+petshop2 = Petshop.new( {'name' => 'great big Pet Emporium', 'type' => 'run of the mill'}, runner )
+petshop3 = Petshop.new( {'name' => 'Illegally imported Pet Emporium', 'type' => 'Exotic'}, runner )
+
 
 petshop1.save()
 petshop2.save()
 petshop3.save()
 
 pet1 = Pet.new( {'name' => 'Algernon', 'type' => 'mouse', 'petshop_id' => 1 }, runner )
-pet2 = Pet.new( {'name' => 'Balthazar', 'type' => 'Monkey', 'petshop_id' => 1 }, runner )
+pet2 = Pet.new( {'name' => 'Balthazar', 'type' => 'Shrew', 'petshop_id' => 1 }, runner )
 
 pet3 = Pet.new( {'name' => 'Jennifer', 'type' => 'Sloth', 'petshop_id' => 2 }, runner )
 pet4 = Pet.new( {'name' => 'Steven Morrissey', 'type' => 'Porcupine', 'petshop_id' => 2 }, runner )
 
-pet5 = Pet.new( {'name' => 'Jimmy', 'type' => 'Shrew', 'petshop_id' => 3 }, runner )
+pet5 = Pet.new( {'name' => 'Jimmy', 'type' => 'Monkey', 'petshop_id' => 3 }, runner )
 pet6 = Pet.new( {'name' => 'Franklyn', 'type' => 'Peregrine Falcon', 'petshop_id' => 3 }, runner )
 
-petshop1.list_pets_from_a_petshop(2)
+
 
 pet1.save()
 pet2.save()
@@ -34,8 +35,18 @@ pet4.save()
 pet5.save()
 pet6.save()
 
-petshop1  .edit_petshop('Petland - Edinburgh', 'Fantastic Pet Emporium - Edinburgh')
+# petshop1.find_petshop_by_id(2)
+
+# petshop1.list_all_petshops
+
+# petshop1.list_pets_from_a_petshop(2)
+
+# petshop1.delete_petshop(2)
+
+# petshop1.edit_petshop('name', 'pet-o-rama', 3)
 
 
-# binding.pry
-# nil
+
+
+binding.pry
+nil
--- models/petshop.rb
+++ models/petshop.rb
@@ -5,6 +5,7 @@ attr_reader(:name, :id)
   def initialize( options, runner )
     @id = options['id'].to_i
     @name = options['name']
+    @type = options['type']
     @runner = runner
   end
 
@@ -15,29 +16,34 @@ attr_reader(:name, :id)
   end
 
   def list_pets_from_a_petshop(id)
-     sql = "SELECT name FROM pets where petshop_id = '#{id}';"
-     pets = @runner.run(sql)
-     all_the_pets = pets.map { |pet| pet["name"] }
-     print all_the_pets   
+    sql = "SELECT name FROM pets WHERE petshop_id = '#{id}';"
+    pets = @runner.run(sql)
+    all_the_pets = pets.map { |pet| pet['name'] }
+    print all_the_pets   
   end
 
-
-
-  def find_petshop_by_id
-    #
+  def find_petshop_by_id(id)
+    sql = "SELECT name FROM petshops WHERE id = '#{id}';"
+    petshop = @runner.run(sql)
+    find_petshop = petshop.map { |shop| shop["name"] }
+    print find_petshop
   end
 
-  def edit_petshop(new_name, old_name)
-    sql = "UPDATE petshops SET #{'name'} = '#{new_name}' WHERE name = '#{old_name}';"
+  def edit_petshop(attribute, new_input, id)
+    sql = "UPDATE petshops SET #{attribute} = '#{new_input}' WHERE id = '#{@id}';"
     @runner.run( sql )
   end
 
-  def delete_petshop
-    #
+  def delete_petshop(id)
+    sql = "DELETE FROM petshops WHERE id = '#{@id}';"
+    @runner.run( sql )
   end
 
-  def list_all_petshops
-    #
+  def list_all_petshops()
+    sql = 'SELECT * FROM petshops'
+    all_shops = @runner.run( sql )
+    list_of_shops = all_shops.map { |shop| shop['name'] }
+    print list_of_shops
   end
 
 
